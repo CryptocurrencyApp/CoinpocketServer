5717f7a406ac5e3f3d06ff28095c1efb1918e427
ahaha0807 <b5331@oic.jp>
2018-02-04 21:41:09 +0900

    ormの設定

--- main.go
+++ main.go
@@ -4,7 +4,14 @@ import (
 	_ "github.com/CryptocurrencyApp/CoinpocketServer/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "cp:cp@/coinpocket?charset=utf8", 30)
+}
 
 func main() {
 	if beego.BConfig.RunMode == "dev" {
