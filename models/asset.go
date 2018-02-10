b8d77f8491883a095c1064c7964444eadf8af1dc
ahaha0807 <b5331@oic.jp>
2018-02-10 18:52:09 +0900

    assetのmodelの再フォーマット

--- models/asset.go
+++ models/asset.go
@@ -10,10 +10,10 @@ import (
 )
 
 type Asset struct {
-	Id       int64  `orm:"auto"`
-	UserId   string `orm:"size(128)"`
-	CoinId   string `orm:"size(128)"`
-	Amount   float64
+	Id     int64  `orm:"auto"`
+	UserId string `orm:"size(128)"`
+	CoinId string `orm:"size(128)"`
+	Amount float64
 }
 
 func init() {
