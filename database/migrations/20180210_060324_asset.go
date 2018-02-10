9bd69410dc624ac92ac85085fbc52ad575e2dd0c
ahaha0807 <b5331@oic.jp>
2018-02-10 18:31:35 +0900

    assetのマイグレーションファイルの作成

---
+++ database/migrations/20180210_060324_asset.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type Asset_20180210_060324 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Asset_20180210_060324{}
+	m.Created = "20180210_060324"
+
+	migration.Register("Asset_20180210_060324", m)
+}
+
+// Run the migrations
+func (m *Asset_20180210_060324) Up() {
+	sql := "CREATE TABLE asset(" +
+		"id int PRIMARY KEY," +
+		"user_id nvarchar(100)," +
+		"coin_id nvarchar(100)," +
+		"amount decimal" +
+		");"
+
+	m.SQL(sql)
+}
+
+// Reverse the migrations
+func (m *Asset_20180210_060324) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	m.SQL("DROP TABLE asset;")
+}
