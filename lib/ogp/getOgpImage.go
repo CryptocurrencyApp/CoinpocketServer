9db1fda3824d1b6d1c9186fd06c5a33a533dcccd
r-miyamae <b5005@oic.jp>
2018-02-16 02:38:40 +0900

    improve getOgpImg

--- lib/ogp/getOgpImage.go
+++ lib/ogp/getOgpImage.go
@@ -7,13 +7,20 @@ import (
 
 func GetOgpImage(article *models.Article) (err error) {
 	doc, _ := goquery.NewDocument(article.Url)
-	doc.Find("meta[property='og:title']").Each(func(_ int, s *goquery.Selection) {
-		property, _ := s.Attr("content")
-		article.SiteTitle = property
-	})
+
+	// ogpがない場合の初期値
+	// 画像はionicにある
+	article.Image = "../../assets/imgs/noImage.png"
+	article.SiteTitle = doc.Find("title").Text()
+	article.SiteName  = doc.Url.Host
+
 	doc.Find("meta[property='og:image']").Each(func(_ int, s *goquery.Selection) {
 		property, _ := s.Attr("content")
 		article.Image = property
+	})
+	doc.Find("meta[property='og:title']").Each(func(_ int, s *goquery.Selection) {
+		property, _ := s.Attr("content")
+		article.SiteTitle = property
 	})
 	doc.Find("meta[property='og:site_name']").Each(func(_ int, s *goquery.Selection) {
 		property, _ := s.Attr("content")
